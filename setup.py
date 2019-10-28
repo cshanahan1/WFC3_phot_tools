b90d0ec53e1bba127963203a7b2bb233c7b97910
Clare Shanahan <cshanahan@stsci.edu>
2019-10-28 14:47:16 -0400

    changed repo in setup script

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from setuptools import setup
 setup(name = 'WFC3_phot_tools',
       description = 'Python photometry tools for WFC3 calibration',
       author = 'Space Telescope Science Institute',
-      url = 'https://github.com/shannnnnyyy/photometry_tools',
+      url = 'https://github.com/shannnnnyyy/WFC3_phot_tools',
       packages = find_packages(),
       install_requires = ['astropy', 'matplotlib', 'numpy', 'photutils'])
