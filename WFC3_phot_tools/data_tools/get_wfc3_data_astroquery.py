83abd327d8b14e2d0be9a33701268587feb11746
Clare Shanahan <cshanahan@stsci.edu>
2020-08-21 14:46:19 -0400

    another astroquery fix

--- WFC3_phot_tools/data_tools/get_wfc3_data_astroquery.py
+++ WFC3_phot_tools/data_tools/get_wfc3_data_astroquery.py
@@ -14,8 +14,8 @@ import shutil
 """ 
 
 
-def query_by_propid_targ_filter(prop_ids, targnames='any', filters='any',
-								file_type='any'):
+def query_by_propid_targ_filter(prop_ids, target_names='any', filters='any',
+								file_types='any'):
 
 	""" Astroquery query for data from `instrument` by target name, filter, 
 	and proposal ID. Returns table of data products of file type(s) in 
@@ -25,14 +25,14 @@ def query_by_propid_targ_filter(prop_ids, targnames='any', filters='any',
 	----------
 	prop_ids : str or list of str
 		Proposal ID(s)
-	targnames : str list of str
+	target_names : str list of str
 		Exact target names that should be returned in query, others
 		will be excluded. All spelling/name variations that might appear in MAST
 		should be provided. If 'any', all available targets will be returned.
 	filters: str of list of str.
 		Filters that should be returned in query, other will be excluded. If
 		'any', all available filters will be returned. 
-	file_type : str or list or 
+	file_types : str or list or 
 		File extention type(s) desired (i.e flt, flc, drz...), as a string for 
 		a single type or a list for many. If 'any', all available file types 
 		will be returned.
@@ -46,8 +46,8 @@ def query_by_propid_targ_filter(prop_ids, targnames='any', filters='any',
 
 	if type(prop_ids) != list:
 		prop_ids = [prop_ids]
-	if targnames == 'any':
-		targnames = '*'
+	if target_names == 'any':
+		target_names = '*'
 	if filters == 'any':
 		filters = '*'
 
@@ -58,18 +58,18 @@ def query_by_propid_targ_filter(prop_ids, targnames='any', filters='any',
 
 		obsTable = Observations.query_criteria(obs_collection='HST', 
 											   proposal_id=prop_id,
-											   target_name=targnames,
+											   target_name=target_names,
 											   filters=filters)
-		if file_type == 'any':
+		if file_types == 'any':
 			query_products = Observations.get_product_list(obsTable)
 		else:
-			if type(file_type) == str:
-				file_type=file_type.upper()
-			if type(file_type) == list:
-				file_type=[x.upper() for x in file_type]
+			if type(file_types) == str:
+				file_types=file_types.upper()
+			if type(file_types) == list:
+				file_types=[x.upper() for x in file_types]
 			query_products = Observations.get_product_list(obsTable)
-			query_products = Observations.filter_products(query_products, 
-							 productSubGroupDescription=file_type)
+			query_products = Observations.filter_products(query_products,
+							 productSubGroupDescription=file_types)
 
 		if len(query_products) == 0:
 			print('No records found in query.')
@@ -81,7 +81,8 @@ def query_by_propid_targ_filter(prop_ids, targnames='any', filters='any',
 		if (i == 0) & (j == 0):
 			query_products_total = query_products
 		else:
-			query_products_total = vstack([query_products, query_products_total])
+			query_products_total = vstack([query_products,
+										   query_products_total])
 		print('{} records found'.format(len(query_products)))
 		j = 1
 
@@ -93,46 +94,47 @@ def query_by_data_id(dataset_ids, file_type):
 	""" Astroquery query by file rootname(s) or ASN ID. Query will return 
 		all records found for IDs in `dataset_ids` list (or string if 
 		single ID) of type `file_type`. `file_type` can be set to a string 
-		('FLT'), a list of strings (['FLT', DRZ']), or 'any'. 
+		('FLT'), a list of strings (['FLT', DRZ']), or 'any'."""
 
-		Parameters
-		----------
-		dataset_ids : str or list of str
-			9-digit dataset id(s). Note, if these end in 'j' or 's', which
-			occasionally happens due to failed observations, the id may not
-			appear in the database. Change these files to end with 'q'
-			instead
-		file_type : str
-			Fits file type (e.g 'flt', 'flc').
+	# initial query for all files in visit, since you can only query by ASN ID 
+	# and `dataset_ids might contain single exposure rootnames
+	if type(dataset_ids) == str:
+		visit_ids = [dataset_ids[0:6] + '*']
+	if type(dataset_ids) == list:
+		visit_ids = list(set([x[0:6]+'*' for x in dataset_ids]))
 
-		Returns
-		-------
-		query_products : `astropy.table.Table`
-			Results of query.
-	"""
+	query_products_total = None 
+	for i, idd in enumerate(visit_ids):
+		obsTable = Observations.query_criteria(obstype='all', 
+											   obs_collection='HST', 
+											   obs_id=idd)
+		if file_type == 'any':
+			query_products = Observations.get_product_list(obsTable)
+		else:
+			if type(file_type) == str:
+				file_type=file_type.upper()
+			if type(file_type) == list:
+				file_type=[x.upper() for x in file_type]
+			query_products = Observations.get_product_list(obsTable)
+			query_products = Observations.filter_products(query_products, 
+							 productSubGroupDescription=file_type)
 
-	obsTable = Observations.query_criteria(obstype='all', 
-										   obs_collection='HST', 
-										   obs_id=dataset_ids)
-	if file_type == 'any':
-		query_products = Observations.get_product_list(obsTable)
-	else:
-		if type(file_type) == str:
-			file_type=file_type.upper()
-		if type(file_type) == list:
-			file_type=[x.upper() for x in file_type]
-		query_products = Observations.get_product_list(obsTable)
-		query_products = Observations.filter_products(query_products, 
-						 productSubGroupDescription=file_type)
+		if i == 0:
+			query_products_total = query_products
+		else:
+			query_products_total = vstack([query_products,
+										   query_products_total])
 
 	# Initially all visit files were returned. Now select only specified IDs
 	remove_rows = []
-	for i, obs_id in enumerate(query_products['obs_id']):
+	for i, obs_id in enumerate(query_products_total['obs_id']):
 		if obs_id not in dataset_ids:
 			remove_rows.append(i)
-	query_products.remove_rows(remove_rows)		
+	query_products_total.remove_rows(remove_rows)		
 
-	return query_products
+	print('{} records found'.format(len(query_products_total)))
+	print(query_products_total)
+	return query_products_total
 
 def download_products(query_products, output_dir=''):
 
@@ -173,3 +175,18 @@ def download_products(query_products, output_dir=''):
 
 		# remove temp directory
 		shutil.rmtree(output_dir + 'temp') # remove mast download dir 
+
+# def get_all_standard_star_flcs():
+# 	"""Gets all staring mode data for photom programs"""
+# 	prop_ids = [11426, 11450, 11557, 11903, 11907, 12090, 
+# 			12333, 12334, 12698, 12699, 12707, 13088, 
+# 			13089, 13096, 13574, 13575, 13584, 13711, 
+# 			14018, 14021, 14382, 14384, 14815, 14883, 
+# 			14992, 15113, 15398, 15399, 15582, 15583]
+# 	prop_ids = [str(x) for x in prop_ids]
+# 	targnames = ['G191B2B'] + ['GD153', 'GD-153'] + ['GD71', 'GD-71'] + \
+# 				['P330E', 'GSC-02581-02323'] + ['GRW+70D5824', 'GRW+70D']
+# 	output_dir = '/grp/hst/wfc3p/cshanahan/phot_group_work/standard_star_data/test'
+# 	query_products = query_by_propid_targname(prop_ids[25:], targnames, 'flc')
+# 	download_products(query_products, output_dir=output_dir)
+
